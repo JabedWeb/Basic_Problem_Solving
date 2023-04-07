94ac576211779b9acbe7393228571f4fbd2b6f5b
jabed366 <jabedhasan231@gmail.com>
2023-04-07 06:04:56 +0600

    print,multiproblem

---
+++ Assignment_1st/multiply.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int A,B;
+    scanf("%d",&A);
+    scanf("%d",&B);
+
+    long long int multy=(long long int)A*B;
+    printf("%lld",multy);
+   
+    return 0;
+}
---
+++ Module_03(Loop)/Practice_Problem/add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int A;
+    int B;
+    scanf("%d %d",&A,&B);
+
+    int sum =A+B;
+    printf("%d",sum);
+
+
+    return 0;
+}
