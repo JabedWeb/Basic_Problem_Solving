b8185b1151754bed57407fb98de048aa1df19e4b
jabed366 <jabedhasan231@gmail.com>
2023-04-09 01:42:33 +0600

    Sopping code

---
+++ Assignment_1st/shopping.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    long long N;
+    scanf("%lld",&N);
+
+    if(N>1000){
+        printf("I will buy Punjabi\n");
+        N=N-1000;
+        if(N>=500){
+            printf("I will buy new shoes\n");
+            printf("Alisa will buy new shoes\n");
+        }
+    }
+    else{
+        printf("Bad luck!");
+    }
+
+
+}
