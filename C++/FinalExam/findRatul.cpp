25a559d7546fb5301ec14582ad98be053688a071
jabed366 <jabedhasan231@gmail.com>
2023-06-02 05:27:46 +0600

    Find Ratul Problem solved

--- C++/FinalExam/findRatul.cpp
+++ C++/FinalExam/findRatul.cpp
@@ -1,3 +1,4 @@
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -23,7 +24,21 @@ int main() {
     string name=uppercase("raTul");
    // cout << name << endl;
 
-    unsigned long long int findPosition = sentence.find(name);
+
+    unsigned long long int findPosition = sentence.length()+1;
+    // if we get the name in the sentence first then we don't give any space before the name
+    if(sentence.find(name) == 0) {
+        findPosition = sentence.find(name+" ");
+    }
+        // if we get the name in the sentence last then we give a space before the name
+    else if(sentence.find(name) == sentence.length()-name.length()) {
+        findPosition = sentence.find(" "+name);
+    }
+            // if we get the name in the sentence middle then we give a space before and after the name
+        else {
+       findPosition = sentence.find(" "+name+" ");
+    }
+
 
     //cout << findPosition << endl;
 
