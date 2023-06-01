e01fd956fd47d30851016b41abf0f7dc208c3190
jabed366 <jabedhasan231@gmail.com>
2023-06-02 03:31:22 +0600

    Replace string Problem

---
+++ C++/FinalExam/replace.cpp
@@ -0,0 +1,63 @@
+/*
+Problem Statement
+
+You will be given two strings S and X. You need to replace all X from string S with a '$' sign.
+
+Input Format
+
+First line will contain T, the number of test cases.
+Next T lines will contain a line with S and X.
+Constraints
+
+1 <= T <= 1000
+1 <= |S|, |X| <= 1000
+|X| <= |S|
+Output Format
+
+For each test cases output the modified string S.
+Sample Input 0
+
+2
+thatsagoodmomentforgoodrelations good
+canyoutellmewhereicanfindheriwillbegreatefultoyouifyoutellmetheanswer you
+Sample Output 0
+
+thatsa$momentfor$relations
+can$tellmewhereicanfindheriwillbegreatefulto$if$tellmetheanswer
+
+
+*/
+
+
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+    int n;
+    cin>>n;
+    string s,x;
+    for(int i=0;i<n;i++){
+    // getline(cin,s);
+    // getline(cin,x);
+    cin>>s>>x;
+    unsigned long long int findPosition = s.find(x);
+    while (findPosition < s.length()) {
+        // cout << findPosition << endl;
+        s.replace(findPosition, x.length(), "$");
+        // to avoid every time find the string from the beginning
+        findPosition = s.find(x, findPosition + x.length());
+        findPosition = s.find(x, findPosition + 1);
+    }
+
+    // after modified string
+    cout << s<<endl;
+    }
+
+    return 0;
+}
--- C++/MidExam/test.cpp
+++ C++/MidExam/test.cpp
@@ -1,21 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Student {
-public:
-    int id;
-
-    Student(int studentId) : id(studentId) {}
-};
-
-Student* getDefaultStudent() {
-    static Student defaultStudent(12345);
-    return &defaultStudent;
-}
-
 int main() {
-    Student* studentPtr = getDefaultStudent();
-   cout << "Default Student ID: " << studentPtr->id <<endl;
-
+    int n;
+    cin>>n;
+    printf("%d\n",n);
     return 0;
 }
