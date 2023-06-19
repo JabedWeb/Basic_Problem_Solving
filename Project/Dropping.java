4ddf52acab0472b8a8399b5c19ad0e9080ded36a
jabed366 <jabedhasan231@gmail.com>
2023-06-20 00:52:08 +0600

    Ball Dropping Problem solved

--- Project/Dropping.java
+++ Project/Dropping.java
@@ -5,11 +5,15 @@ public class Dropping {
 
 // Recursive function to calculate the final position of the ball
 public static int getFinalPosition(int currentDepth, int currentBallNumber) {
+    System.out.println("Current depth: " + currentDepth + ", current ball number: " + currentBallNumber);
+   
+     //if the current depth is equal to or greater than the bound. If it is, it means the ball has reached the final depth, so the current depth is returned as the final position of the ball.
     if (currentDepth >= bound) {
         return currentDepth;
     }
     int leftChild = currentDepth * 2;
     int rightChild = leftChild + 1;
+    System.out.println("Left child: " + leftChild + ", right child: " + rightChild);
     if (currentBallNumber % 2 == 0) {
         return getFinalPosition(rightChild, currentBallNumber / 2);
     } else {
