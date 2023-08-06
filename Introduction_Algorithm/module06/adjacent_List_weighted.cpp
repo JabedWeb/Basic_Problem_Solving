ce1079967503e7f24ac0092a49cd0dd46dead64d
jabed366 <jabedhasan231@gmail.com>
2023-08-07 03:09:57 +0600

    adjacent list with weighted

---
+++ Introduction_Algorithm/module06/adjacent_List_weighted.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N=1e5+3;
+vector <pair<int,int> >adj[N];
+
+int main() {
+    int v,e;
+    cin>>v>>e;
+
+
+    for(int i=0;i<e;i++){
+        int x,y,w;
+        cin>>x>>y>>w;
+
+        adj[x].push_back({y,w});
+        //adj[x].push_back(make_pair(y,w))
+    }
+    for(int i=1;i<=v;i++){
+        cout<<"List " << i << " : ";
+        for(auto j:adj[i]){
+            cout<< "("<< j.first<<" ," <<j.second <<"), ";
+        }
+        cout<<endl;
+    }
+}
