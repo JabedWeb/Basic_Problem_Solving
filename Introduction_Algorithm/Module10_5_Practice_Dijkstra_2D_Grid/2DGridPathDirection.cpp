959ec5410b1b32faeb8c1a07b8d27a9fe3eb7fa8
jabed366 <jabedhasan231@gmail.com>
2023-08-15 08:13:01 +0600

    2D Grid Practice Problem solved

--- Introduction_Algorithm/Module10_5_Practice_Dijkstra_2D_Grid/2DGridPathDirection.cpp
+++ Introduction_Algorithm/Module10_5_Practice_Dijkstra_2D_Grid/2DGridPathDirection.cpp
@@ -1,3 +1,39 @@
+/*
+
+Question: You are given a 2D grid which will contain only the characters ‘s’, ‘.’, ‘x’ and ‘e’. The size of the grid is N*M squares, where ‘s’ means where you should start, ‘.’ means there is a path, ‘x’ means there is no path and ‘e’ means where you should stop. You can walk left, right, up, and down through the cell of squares. You need to tell the minimum number of steps you need to go from ‘S’ to ‘E’ and also you need to print the path in the form of R(Right), L(Left), U(Up) and D(Down) from source to destination. There will be exactly one ‘s’ and one ‘e’. If there is no path, print -1.
+Sample Input
+Sample Output
+6 5
+.s.x.
+...x.
+..x..
+..x..
+..xex
+.....
+8
+DDDDDRRU
+5 5
+.s.x.
+...x.
+..x..
+..x..
+..xex
+-1
+5 5
+.....
+.....
+.x..e
+s.x..
+....x
+7
+UURRRRD
+
+
+
+*/
+
+
+
 #include <bits/stdc++.h>
 using namespace std;
 int n,m;
@@ -77,10 +113,10 @@ int main() {
     }
     path.push_back({si,sj});
     reverse(path.begin(),path.end());
-    for(pii d : path){
-        cout <<d.first << " " <<d.second <<endl;
-    }
-    cout <<path.size() <<endl;
+    // for(pii d : path){
+    //     cout <<d.first << " " <<d.second <<endl;
+    // }
+    // cout <<path.size() <<endl;
 
     for(int i =1;i<path.size();i++){
         if(path[i-1].first ==path[i].first){
