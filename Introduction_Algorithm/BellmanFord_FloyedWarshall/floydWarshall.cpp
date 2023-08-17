b1d0d2cd452a7c6cf9e5ff456f627d66cc739105
jabed366 <jabedhasan231@gmail.com>
2023-08-17 08:00:02 +0600

    number of islands

--- Introduction_Algorithm/BellmanFord_FloyedWarshall/floydWarshall.cpp
+++ Introduction_Algorithm/BellmanFord_FloyedWarshall/floydWarshall.cpp
@@ -1,3 +1,24 @@
+/*
+5
+7
+1 2 2
+1 3 6
+2 3 1
+3 4 4
+4 2 6
+2 5 3
+5 4 9
+*/
+
+/*
+0 2 3 7 5 
+X 0 1 5 3 
+X 10 0 4 13 
+X 6 7 0 9 
+X 15 16 9 0 
+*/
+
+
 #include <bits/stdc++.h>
 using namespace std;
 const int N =10e3+5;
@@ -39,7 +60,16 @@ int main() {
     }
 
     printMatrix();
+    for(int k=1;k<=1;k++){ 
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                d[i][j]=min(d[i][j],d[i][k]+d[k][j]);
+            }
+        }
+    }
+    cout <<endl;
 
+    printMatrix();
     for(int k=1;k<=n;k++){
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
