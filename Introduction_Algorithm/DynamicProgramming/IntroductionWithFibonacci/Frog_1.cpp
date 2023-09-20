b08f6bbad796dcdba3dd362109e200f64779c217
Jabedweb <jabedhasan231@gmail.com>
2023-09-20 19:44:15 +0600

    frog1 problem solved

---
+++ Introduction_Algorithm/DynamicProgramming/IntroductionWithFibonacci/Frog_1.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N=10E5+5;
+int dp[N];
+
+int solve(int arr[],int n){
+    if(n==1) return 0;
+    if(dp[n]!=-1) return dp[n];
+    else if(n==2) return dp[n]=abs(arr[2]-arr[1]);
+    else{
+        int choice1=solve(arr,n-1)+abs(arr[n]-arr[n-1]);
+        int choice2=solve(arr,n-2)+abs(arr[n]-arr[n-2]);
+        return dp[n]=min(choice1,choice2);
+    }
+}
+
+int main() {
+    int n;
+    cin>>n;
+    int arr[n+1];
+    for(int i =1;i<=n;i++){
+        dp[i]=-1;
+        int value;
+        cin>>value;
+        arr[i]=value;
+    }
+    cout<<solve(arr,n);
+}
