c5ed988eba3bba30ec7e3c1e9075a40357b2c0fd
Jabedweb <jabedhasan231@gmail.com>
2023-09-22 19:58:00 +0600

    water problem solved

--- Water.cpp
+++ Water.cpp
@@ -1,7 +1,28 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <climits>
+
 using namespace std;
 
-typedef pair <long long,int> pii;
+void water(vector<long long> heights) {
+    int n = heights.size();
+    long long max1 = -1, max2 = -1;
+    int maxIndex1 = -1, maxIndex2 = -1;
+
+    for (int i = 0; i < n; i++) {
+        if (heights[i] > max1) {
+            max2 = max1;
+            maxIndex2 = maxIndex1;
+            max1 = heights[i];
+            maxIndex1 = i;
+        } else if (heights[i] > max2) {
+            max2 = heights[i];
+            maxIndex2 = i;
+        }
+    }
+
+    cout << min(maxIndex1, maxIndex2)<< " " << max(maxIndex1, maxIndex2) << endl;
+}
 
 int main() {
     int t;
@@ -11,17 +32,12 @@ int main() {
         int n;
         cin >> n;
 
-        vector<pii> hight_Index(n);
+        vector<long long> heights(n);
         for (int i = 0; i < n; i++) {
-            cin >> hight_Index[i].first;
-            hight_Index[i].second = i;
+            cin >> heights[i];
         }
 
-       
-        sort(hight_Index.begin(), hight_Index.end(), greater<pair<int, int>>());
-
-        cout << min(hight_Index[0].second, hight_Index[1].second)  << " "
-             << max(hight_Index[0].second, hight_Index[1].second) << endl;
+        water(heights);
     }
 
     return 0;
